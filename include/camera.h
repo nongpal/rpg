a2ae384b950dc1aa7cb44844ffe31dbe11d22307
0xFA99 <fajar.nur.h@pm.me>
2026-02-18 04:52:28 +0700

    refactor: add camera zoom with mouse wheel support

--- include/camera.h
+++ include/camera.h
@@ -2,13 +2,14 @@
 #define CAMERA_H
 
 #include "raylib.h"
-#include "player.h"
 #include "tilemap.h"
+#include "player.h"
 
 typedef struct {
     Camera2D camera;
     float followSpeed;
     float zoom;
+    float zoomTarget;
     Vector2 offset;
     bool smoothFollow;
     Vector2 boundsMin;
@@ -16,7 +17,7 @@ typedef struct {
 } GameCamera;
 
 GameCamera InitGameCamera(int screenWidth, int screenHeight);
-void UpdateCamera2D(GameCamera* camera, const Player* player, const Tilemap* map);
+void UpdateCamera2D(GameCamera* camera, const Player* player, const Tilemap* map, float frameTime);
 void SetCameraBounds(GameCamera* camera, const Tilemap* map);
 void SetCameraZoom(GameCamera* camera, float zoom);
 
